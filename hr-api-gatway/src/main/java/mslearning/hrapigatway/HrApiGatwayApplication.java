89618ea2dac757cff37876d76ca57f611bd26cb7
Lucas-B-Teixeira <lucastgrimera1@gmail.com>
2023-09-28 14:03:33 -0300

    Project hr-api-gatway Created

---
+++ hr-api-gatway/src/main/java/mslearning/hrapigatway/HrApiGatwayApplication.java
@@ -0,0 +1,13 @@
+package mslearning.hrapigatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatwayApplication.class, args);
+	}
+
+}
---
+++ hr-api-gatway/src/test/java/mslearning/hrapigatway/HrApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package mslearning.hrapigatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
