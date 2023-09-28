73674987133798f1217c9322c5dc0e7e2feb12b0
Lucas-B-Teixeira <lucastgrimera1@gmail.com>
2023-09-28 13:23:19 -0300

    Resilience4j implemented circuitBreak and fallBack

--- hrpayroll/src/main/java/mslearning/hrpayroll/controller/PaymentController.java
+++ hrpayroll/src/main/java/mslearning/hrpayroll/controller/PaymentController.java
@@ -1,9 +1,9 @@
 package mslearning.hrpayroll.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import mslearning.hrpayroll.model.PaymentModel;
 import mslearning.hrpayroll.services.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 @AllArgsConstructor
 public class PaymentController {
+
     private final PaymentService paymentService;
 
+    @CircuitBreaker(name = "hr-worker", fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<PaymentModel> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         PaymentModel paymentModel = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(paymentModel);
+    }
+
+    public ResponseEntity<PaymentModel> getPaymentAlternative(Long workerId, Integer days, Throwable throwable){
+        PaymentModel paymentModel = new PaymentModel("Brann", 400.0, days);
         return ResponseEntity.ok(paymentModel);
     }
 }
--- hrpayroll/src/main/java/mslearning/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/mslearning/hrpayroll/services/PaymentService.java
@@ -1,24 +1,33 @@
 package mslearning.hrpayroll.services;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import mslearning.hrpayroll.dto.WorkerDTO;
 import mslearning.hrpayroll.feignClients.WorkerFeignClient;
 import mslearning.hrpayroll.model.PaymentModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Service
 @AllArgsConstructor
 public class PaymentService {
 
+
     private final WorkerFeignClient workerFeignClient;
 
+    //@CircuitBreaker(name = "hr-worker", fallbackMethod = "getPaumentAlternative")
     public PaymentModel getPayment(long workerId, int days){
 
         WorkerDTO worker = workerFeignClient.findById(workerId).getBody();
 
         return new PaymentModel(worker.getName(), worker.getDailyIncome(), days);
     }
+
+//    public PaymentModel getPaumentAlternative(Long workerId, Integer days){
+////        PaymentModel paymentModel = new PaymentModel("Brann", 400.0, days);
+//        return new PaymentModel("Brann", 400.0, days);
+//    }
 
 }
