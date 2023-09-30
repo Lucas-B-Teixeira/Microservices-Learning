e47d776a028a4fb126d44d5e59c74200710dd4bf
Lucas-B-Teixeira <lucastgrimera1@gmail.com>
2023-09-30 17:46:26 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/mslearning/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package mslearning.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
