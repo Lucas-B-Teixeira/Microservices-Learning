a44391bb9a90efbe07ce4ab94da973a4a509fccd
Lucas-B-Teixeira <lucastgrimera1@gmail.com>
2023-10-04 18:07:40 -0300

    db config server

--- hrwork/src/main/java/mslearning/hrwork/controller/WorkerController.java
+++ hrwork/src/main/java/mslearning/hrwork/controller/WorkerController.java
@@ -23,20 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WorkerController {
 
-    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
-
-    @Value("${test.config}")
-    private String TEST_CONFIG;
-
     private final WorkerService workerService;
-
-    private final Environment env;
-
-    @GetMapping(value = "/configs")
-    public ResponseEntity<Void> getConfigs(){
-        logger.info("CONFIG " + TEST_CONFIG);
-        return ResponseEntity.noContent().build();
-    }
 
     @GetMapping()
     public ResponseEntity<List<WorkerDTO>> findAll(){
@@ -45,7 +32,6 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<WorkerDTO> findById(@PathVariable Long id){
-        logger.info("PORT = " + env.getProperty("local.server.port"));
 
         return ResponseEntity.ok(workerService.findByID(id));
     }
